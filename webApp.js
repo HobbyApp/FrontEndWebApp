570d2aa363d9a21f64a0d9e20cca4af000cbdf5d
theConm4n <42720400+theConm4n@users.noreply.github.com>
2018-09-15 12:43:35 -0500

    Add files via upload

--- webApp.js
+++ webApp.js
@@ -54,7 +54,7 @@ $(document).ready(function captureTableRowClick() {
           a = div.getElementsByTagName("a");
           for (i = 0; i < a.length; i++) {
             if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-              a[i].style.display = "";
+              a[i].style.display = "show";
             } else {
               a[i].style.display = "none";
             }
