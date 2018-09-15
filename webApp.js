5d3a606f4bd68ae988e967621c2cdb4cf9a982a3
theConm4n <42720400+theConm4n@users.noreply.github.com>
2018-09-15 12:22:25 -0500

    Add files via upload

--- webApp.js
+++ webApp.js
@@ -18,7 +18,7 @@ function openFilter() {
      y.style.display = "none";
    }
 }
-// Ignore this for now
+//search funtion for hobby filter option
 $(document).ready(function captureTableRowClick() {
             $('tr').click(function () {
                 var tableData = $(this).children('td').map(function returnTableDataText() {
@@ -40,3 +40,23 @@ $(document).ready(function captureTableRowClick() {
             });
 
         });
+        /* When the user clicks on the button,
+        toggle between hiding and showing the dropdown content */
+        function openHobbyFilter() {
+          document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        function filterFunction() {
+          var input, filter, ul, li, a, i;
+          input = document.getElementById("myInput");
+          filter = input.value.toUpperCase();
+          div = document.getElementById("myDropdown");
+          a = div.getElementsByTagName("a");
+          for (i = 0; i < a.length; i++) {
+            if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+              a[i].style.display = "";
+            } else {
+              a[i].style.display = "none";
+            }
+          }
+        }
