042c6651c91041f29e6a0b0ecbc9eea43830bf43
theConm4n <42720400+theConm4n@users.noreply.github.com>
2018-10-10 23:30:33 -0400

    Add files via upload

--- profile.php
+++ profile.php
@@ -14,28 +14,47 @@
   <div class="container">
     <img id="profilePic" src="https://placeimg.com/240/240/people/grayscale" class="ribbon">
     <button type="button" id="editButton" onclick="openEditInfoForm()"></button>
+
   </div>
   <!--User form that displays current user profile information -->
   <center><div id="userProfile">
     <label for="name">Hello! Welcome to your profile</label>
       <var id="firstName">Conor</var>
+      <var id="middleName">Collin</var>
       <var id="lastName">Steward</var><br>
     <label for="userHobbies">Your Hobbies:</label>
-      <ul id="userHobbies">
-      <li>Hobby</li>
-      <li>Hobby</li>
-      <li>Hobby</li>
-      <li>Hobby</li>
-      </ul>
+    <?php
+        $db_host = '127.0.0.1'; // Server Name
+        $db_user = 'root'; // Username
+        $db_pass = 'password'; // Password
+        $db_name = 'mydb'; // Database Name
+
+        $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+        if (!$conn) {
+          die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+        }
+        $hobbyQuery = "SELECT hobbies FROM hobbies";
+        $hobbyResult = mysqli_query($conn, $hobbyQuery);
+//a list of added hobbies
+        echo '<ul id="userHobbies">';
+
+        while ($row = mysqli_fetch_array($hobbyResult)) {
+          echo '<li value="'.$row['hobbies'].'">'.$row['hobbies'].'</li>';
+        }
+        echo '</ul>';
+    ?>
 <!-- Opens to allow user to edit their personal information-->
 </div></center>
   <form style="display:none;" id="userInfoForm">
     <fieldset>
     <center><p>Edit User Information</p></center>
-    <input type="text" value="First Name"><br>
-    <input type="text" value="Last Name"><br>
-    <input type="text" value="Add Hobby Here">
-          <input list="readFormHobbyList" name="readFormHobbyList">
+    <input type="text" placeholder="Change First Name" name="firstName">
+    <input type="text" placeholder="Change Middle Name" name="middleName">
+    <input type="text" placeholder="Change Last Name" name="lastName"><br>
+    <input type="text" placeholder="Change Username" name="username"><br>
+    <input type="password" placeholder="Password" id="password">
+    <input type="password" placeholder="Confirm Password" id="confirm_password"><br>
+    <input list="readFormHobbyList" name="readFormHobbyList" placeholder="Add New Hobby">
         <?php
             $db_host = '127.0.0.1'; // Server Name
             $db_user = 'root'; // Username
@@ -46,44 +65,55 @@
             if (!$conn) {
               die ('Failed to connect to MySQL: ' . mysqli_connect_error());
             }
-            $hobbyQuery = "SELECT hobby FROM hobbies";
+
+            $hobbyQuery = "SELECT hobbies FROM hobbies";
             $hobbyResult = mysqli_query($conn, $hobbyQuery);
 
             echo '<datalist name="readFormHobbyList" id="readFormHobbyList">';
 
             while ($row = mysqli_fetch_array($hobbyResult)) {
-              echo '<option value="'.$row['hobby'].'">'.$row['hobby'].'</option>';
+              echo '<option value="'.$row['hobbies'].'">'.$row['hobbies'].'</option>';
             }
             echo '</datalist>';
+
+            // updates users name, username and password
+            $firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
+            $middleName = mysqli_real_escape_string($conn, $_POST['middleName']);
+            $lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $password = mysqli_real_escape_string($conn, $_POST['password']);
+            // Capturing dateOfBirth from the user
+            $dateOfBirth= mysqli_real_escape_string($conn, $_POST['dateOfBirth']);
+            // Converting date to MySQL readable format - you must use the $date variable, not the one above or MySQL will throw an error
+            $date=date("Y-m-d",strtotime($dateOfBirth));
+            $newHobby = mysqli_real_escape_string($conn, $_POST['readFormHobbyList']);
+
+
+
+            if (isset($_POST['updateUserInfo'])) {
+
+              // the path to store the uploaded image
+              $target = "images/".basename($_FILES['image']['name']);
+
+              // Get all the submitted data from the form
+              $image = $_FILES['image']['name'];
+
+              $hobbyTarget = $_POST['hobbySelector'];
+
+              // Attempt insert query execution
+              $sql = "UPDATE users (firstName, middleName, lastName, dateOfBirth, profilePicture, username, password, hobbies)
+                      VALUES ('$firstName', '$middleName', '$lastName', '$dateOfBirth', '$image', '$username', '$password', '$newHobby')";
+
+
+
+              mysqli_query($conn, $sql); //stores the submitted data into the database table
+            }
         ?>
-      </input>
+      </input><br>
 
-    <input list="writeFormHobbyList" name="writeFormHobbyList">
-    <?php
-    /*
-        $db_host = '127.0.0.1'; // Server Name
-        $db_user = 'root'; // Username
-        $db_pass = 'password'; // Password
-        $db_name = 'mydb'; // Database Name
-
-        $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-        if (!$conn) {
-          die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-        }
-        $writeSQL = "UPDATE hobby SET hobbies";
-        $writeResult = mysqli_query($conn, $writeSQL);
-
-        echo '<datalist name="writeFormHobbyList" id="writeFormHobbyList">';
-
-        while ($row = mysqli_fetch_array($hobbyResult)) {
-          echo '<option value="'.$row['hobby'].'">'.$row['hobby'].'</option>';
-        }
-        echo '</datalist>'; */
-    ?>
-  </input>
     <label for="inputBirthday">What is your date of birth?</label><br>
     <input type="date" id="inputBirthday"></input><br>
-    <center><input type="submit"></center>
+    <center><input type="submit" name="updateUserInfo"></center>
   </fieldset></form>
 </body>
 
